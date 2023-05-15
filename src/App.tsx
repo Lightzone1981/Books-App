64fa7e9d1bb67239bd9fa37a08563983a4c0c145
Evgeny Lebedevsky <115792378+Lightzone1981@users.noreply.github.com>
2023-05-15 19:53:02 +0300

    first commit - start app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { BookCardsGrid } from './components';
+
+
+function App() {
+  return (
+    <div className="App">
+      <BookCardsGrid/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/BookCardsGrid/BookCardsGrid.tsx
@@ -0,0 +1,7 @@
+import "./BookCardsGrid.css";
+const BookCardsGrid = () => {
+	//   const authorizedUserName = useSelector((state: IStoreState) => state.user.authorizedUser.username);
+
+	return <div className="cards-container">Hi</div>;
+};
+export default BookCardsGrid;
